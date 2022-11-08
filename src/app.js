33853a742cb4ff066f9ac65b2623443b4d0a0384
IOGANESSON <112421290+IOGANESSON@users.noreply.github.com>
2022-11-09 01:50:44 +0330

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,8 +1,43 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return (day = `${day} ${hours}:${minutes}`);
+}
+
 function showWeatherTemprature(response) {
   console.log(response.data);
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = response.data.main.temp;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let temperatureElement = document.querySelector("#mainTemp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windelement = document.querySelector("#wind");
+  windelement.innerHTML = Math.round(response.data.wind.speed);
+  let weatherElement = document.querySelector("#weather");
+  weatherElement.innerHTML = response.data.weather[0].main;
+  let dayElement = document.querySelector("#day");
+  dayElement.innerHTML = formatDate(response.data.dt * 1000);
 }
+let city = "behshahr";
 let apiKey = `c8784affc7f48d97a3ffacdb8dec2d4c`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=paris&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
 axios.get(apiUrl).then(showWeatherTemprature);
